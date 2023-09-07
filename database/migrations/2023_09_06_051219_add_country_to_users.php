54b4d9001968f21088777f9fc7c4120044e1296e
Juan Quintanilla <juan.quintanilla@interseguro.com.pe>
2023-09-06 23:39:59 -0500

    fix en el migrate de users

--- database/migrations/2023_09_06_051219_add_country_to_users.php
+++ database/migrations/2023_09_06_051219_add_country_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('country', 100);
+            $table->string('country', 100)->default('Perú');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('country')->default('Perú');
+            $table->dropColumn('country');
         });
     }
 };
