a3106d0dfd8a342d1507f99f073a717ce8d4a806
Juan Quintanilla <juan.quintanilla@interseguro.com.pe>
2023-09-11 16:59:57 -0500

    agregueé la docu

--- app/Http/Controllers/DeezerController.php
+++ app/Http/Controllers/DeezerController.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Http;
 
 class DeezerController extends BaseController
 {
+    /**
+     * Search album by id
+     *
+     * @urlParam id integer required The ID of the album. Example: 302127
+     * @authenticated
+     * @return AlbumObject
+     */
     public function albumSearch(Request $request)
     {
         $albumId = $request->id;
@@ -19,6 +26,13 @@ class DeezerController extends BaseController
         return $this->sendResponse($data, 'Albums by id - An album object');
     }
 
+    /**
+     * Search artist by id
+     *
+     * @urlParam id integer required The ID of the artist. Example: 27
+     * @authenticated
+     * @return ArtistObject
+     */
     public function artistSearch(Request $request)
     {
         $artistId = $request->id;
@@ -30,6 +44,13 @@ class DeezerController extends BaseController
         return $this->sendResponse($data, 'Artist by id - An artist object');
     }
 
+    /**
+     * Search tracks by criteria
+     *
+     * @urlParam q string required The criteria for search tracks. Example: eminem
+     * @authenticated
+     * @return TracksObjectList
+     */
     public function songSearch(Request $request)
     {
         $search = $request->q;
